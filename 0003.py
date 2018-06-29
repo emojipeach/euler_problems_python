dfcff4dba2334210f2c9d6942dd397ee46e46a17
emojipeach <37852121+emojipeach@users.noreply.github.com>
2018-06-29 23:44:29 +0200

    Create 0003.py

---
+++ 0003.py
@@ -0,0 +1,26 @@
+"""The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?"""
+
+def find_factors(a):
+	for i in range(1,a-1):
+		if a % i == 0:
+			factors.append(i)
+
+def test_for_prime(b):
+	for i in range(3,b):
+		if b % i == 0:
+			break
+	else:
+		primes.append(b)
+
+def highest_prime_factor(x):
+	find_factors(x)
+	for i in factors:
+		test_for_prime(i)
+	print(max(primes))
+
+factors = []
+primes = []
+
+highest_prime_factor(600851475143)
